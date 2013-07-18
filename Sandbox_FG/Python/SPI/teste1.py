6a27d16d7614bf13fe00d0510ef52b29a18b5572
Metacriacoes Development <development@metacriacoes.com>
2013-07-18 11:41:51 +0000

    Modified teste1.py

--- Sandbox_FG/Python/SPI/teste1.py
+++ Sandbox_FG/Python/SPI/teste1.py
@@ -5,6 +5,7 @@ dev = spidev.SpiDev(0,0) # open up /dev/spidev0.1
 
 dev.max_speed_hz = 10000
 dev.lsbfirst = False
+dev.bits_per_word = 16
 """
 dev.mode = 1
 dev.max_speed_hz = 500000
@@ -13,7 +14,7 @@ dev.cshigh = False
 dev.threewire = False
 dev.loop = False # loop is "loopback"
 
-dev.bits_per_word = 8
+
 """
 #dev.xfer([0x0000])
 #dev.xfer([0x80,0x20])
