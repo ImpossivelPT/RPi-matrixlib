10806e0fd2f136ccc14ee9ae936d5f0c0a42c850
Metacriacoes Development <development@metacriacoes.com>
2013-07-18 11:33:16 +0000

    Modified teste1.py

--- Sandbox_FG/Python/SPI/teste1.py
+++ Sandbox_FG/Python/SPI/teste1.py
@@ -2,8 +2,9 @@ import spidev
 import time
 
 dev = spidev.SpiDev(0,0) # open up /dev/spidev0.1
-"""
+
 dev.max_speed_hz = 10000
+"""
 dev.mode = 1
 dev.max_speed_hz = 500000
 dev.cshigh = False
@@ -17,19 +18,11 @@ dev.bits_per_word = 8
 #dev.xfer([0x80,0x20])
 #dev.xfer([0x80,0x60])
 #dev.xfer([0xA0,0x3C])
-"""
-dev.cshigh = True
-dev.cshigh = False
-dev.writebytes([1025])
+dev.writebytes([0x104])
 
-dev.cshigh = True
-dev.cshigh = False
-dev.writebytes([1027])
+dev.writebytes([0x100C])
 
-dev.cshigh = True
-dev.cshigh = False
-dev.writebytes([10255])
-"""
+dev.writebytes([0xA03C])
 
     
 #print dev.bits_per_word
