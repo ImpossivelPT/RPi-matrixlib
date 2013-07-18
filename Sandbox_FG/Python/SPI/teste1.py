af02134a12bc1d5d9abb56e13877bb0c20c54816
Metacriacoes Development <development@metacriacoes.com>
2013-07-18 10:38:47 +0000

    Modified teste1.py

--- Sandbox_FG/Python/SPI/teste1.py
+++ Sandbox_FG/Python/SPI/teste1.py
@@ -10,14 +10,14 @@ dev.lsbfirst = False
 dev.threewire = False
 dev.loop = False # loop is "loopback"
 """
-dev.bits_per_word = 16
+dev.bits_per_word = 8
 
 #dev.xfer([0x0000])
 #dev.xfer([0x80,0x20])
 #dev.xfer([0x80,0x60])
 #dev.xfer([0xA0,0x3C])
 while True:
-    dev.xfer2([0xAA,0xAA])
+    dev.xfer2([5])
 
 #print dev.bits_per_word
 
